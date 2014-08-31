26b7356b238f8349f062769c02ae5914dea2cc82
Jean-Marc Collin <jm.collin.78@gmail.com>
2014-08-31 19:15:35 +0200

    Add jmcnetEmailTemplate + formatting

--- js/jmcnetEmailTemplate.js
+++ js/jmcnetEmailTemplate.js
@@ -4,35 +4,101 @@
  * A module for generating email templates. This module is based on 'ejs'
  */
 
-var _ = require('lodash'),
+var
+// _ = require('lodash'),
+    fs = require('fs'),
     log = require('log4js').getLogger('jmcnet.emailTemplate'),
-    util = require('util');
+    util = require('util'),
+    jmcnetException = require('../js/jmcnetException.js'),
+    ejs = require('ejs');
 
-var BASE_EXCEPTION_NAME = 'BaseException';
-var TECHNICAL_EXCEPTION_NAME= 'TechnicalException';
-var FUNCTIONAL_EXCEPTION_NAME= 'FunctionalException';
+require('../js/jmcnetDate.js');
+
+var gLstMailTpl = {};
+var gLang = 'fr';
 
 /**
- * Loads an email template by 
- * @param message String The exception message
- * @param parameters Array the array of parameters
+ * Construct a new EmailTemplate from a templateName, a subject template name and a bodyTemplate
  */
-function BaseException(message, parameters) {
-    log.trace('BaseException CTOR message="%s" parameters="%s"', message, parameters);
-    this.message = message || '';
-    if (_.isEmpty(parameters)) {
-        this.parameters = [];
-    }
-    else {
-        this.parameters = _.isArray(parameters) ? parameters : [ parameters ];
+function EmailTemplate(templateName, subjectTemplate, bodyTemplate) {
+    log.trace('Constructing a new template "%s"', templateName);
+    this.subjectTemplate = subjectTemplate;
+    this.bodyTemplate = bodyTemplate;
+    this.initTemplate(templateName);
+}
+
+EmailTemplate.prototype.initTemplate = function (templateName) {
+    log.trace('Initializing the template "%s"', templateName);
+    this.subjectCompile = ejs.compile(this.subjectTemplate);
+    this.bodyCompile = ejs.compile(this.bodyTemplate);
+    gLstMailTpl[templateName] = this;
+    log.trace('Template list is now "%s"', util.inspect(gLstMailTpl));
+    return this;
+};
+
+/**
+ * Renders the subjects template for an EmailTemplate
+ * @param context an object describing the context
+ * @param lang fr|en
+ * @return the String containing the rendered subject
+ */
+EmailTemplate.prototype.renderSubject = function (context, lang) {
+    if (lang) gLang = lang;
+    return this.subjectCompile(context);
+};
+EmailTemplate.prototype.renderBody = function (context, lang) {
+    if (lang) gLang = lang;
+    return this.bodyCompile(context);
+};
+
+function getEmailTemplate(templateName) {
+    return gLstMailTpl[templateName];
+}
+
+function getLstTemplates() {
+    return gLstMailTpl;
+}
+
+function resetEmailTemplates() {
+    gLstMailTpl = {};
+}
+
+/**
+ * Create a new EmailTemplate from a filename containing the body
+ * @param templateName String the name of the template to be created
+ * @param subjectTemplate String the template of the email's subject
+ * @param bodyFileName String the file path and name containing the template body
+ * @return a new instance of EmailTemplate
+ */
+function loadEmailTemplateFromFile(templateName, subjectTemplate, bodyFileName) {
+    log.trace('Loading template "%s" from file "%s"', templateName, bodyFileName);
+    try {
+        var body = fs.readFileSync(bodyFileName, 'utf8');
+        body.replace(/\r/g, '');
+        return new EmailTemplate(templateName, subjectTemplate, body);
+    } catch (err) {
+        log.error('Impossible to load EmailTemplate from file. Err is "%s"', util.inspect(err));
+        throw new jmcnetException.TechnicalException(err.message, [bodyFileName]);
     }
 }
 
+/**
+ * An EJS filter to display a date correctly
+ */
+ejs.filters.toLocaleDateString = function(d) {
+    log.trace('Into ejs.filters.toLocaleDateString');
+    if (gLang === 'fr') {
+        return d.format('shortDateFr');
+    }
+    else {
+        return d.format('shortDateEn');
+    }
+};
+
 module.exports = {
-    BaseException : BaseException,
-    TechnicalException : TechnicalException,
-    FunctionalException : FunctionalException,
-    BASE_EXCEPTION_NAME : BASE_EXCEPTION_NAME,
-    TECHNICAL_EXCEPTION_NAME : TECHNICAL_EXCEPTION_NAME,
-    FUNCTIONAL_EXCEPTION_NAME : FUNCTIONAL_EXCEPTION_NAME
+    EmailTemplate: EmailTemplate,
+    loadEmailTemplateFromFile: loadEmailTemplateFromFile,
+    getLstTemplates: getLstTemplates,
+    getEmailTemplate: getEmailTemplate,
+    resetEmailTemplates: resetEmailTemplates
 };
---
+++ test/testEmailTemplate.js
@@ -0,0 +1,116 @@
+/* jshint expr:true*/
+'use strict';
+
+/**
+ * Unit test file for testing the EmailTemplate module
+ * Author : Jean-Marc Collin
+ */
+
+/* Module dependencies */
+var expect = require('chai').expect; // jshint ignore:line
+var assert = require('assert'); // jshint ignore:line
+var log = require('log4js').getLogger('jmcnet.emailTemplate'),
+    // jmcnetException = require('../js/jmcnetException.js'),
+    jmcnetEmailTemplate = require('../js/jmcnetEmailTemplate.js'),
+    util = require('util');
+
+// The tests
+describe.only('<JMCNet EmailTemplate Unit Test>', function () {
+    before(function (done) {
+        jmcnetEmailTemplate.resetEmailTemplates();
+        done();
+    });
+    describe('Creating and retrieve a new template from String', function () {
+        it('should be possible to create and retrieve a new EmailTemplate from Stings', function (done) {
+            log.debug('Test create and retrieve a EmailTemplate from String');
+            expect(jmcnetEmailTemplate.getLstTemplates()).to.be.empty;
+            try {
+                var tpl = new jmcnetEmailTemplate.EmailTemplate('template1', 'subject', 'body');
+                expect(tpl).to.exist;
+
+            } catch (err) {
+                assert.fail('we should not be there');
+            }
+            expect(jmcnetEmailTemplate.getLstTemplates()).to.be.not.empty;
+            expect(jmcnetEmailTemplate.getLstTemplates().template1).to.exist;
+            expect(jmcnetEmailTemplate.getLstTemplates().template1.subjectTemplate).to.equal('subject');
+            expect(jmcnetEmailTemplate.getLstTemplates().template1.bodyTemplate).to.equal('body');
+
+            var tpl1 = jmcnetEmailTemplate.getEmailTemplate('template1');
+            expect(tpl1).to.deep.equal(jmcnetEmailTemplate.getLstTemplates().template1);
+            done();
+        });
+    });
+
+    describe('Rendering the subject and body Test :', function () {
+        var tpl;
+        before(function (done) {
+            jmcnetEmailTemplate.resetEmailTemplates();
+            tpl = new jmcnetEmailTemplate.EmailTemplate('template1', 'subject <%= title %>', 'body <%=body%>');
+            done();
+        });
+        it('should be possible to render the subject and body', function (done) {
+            log.debug('Test Render subject');
+            var subject, body;
+            try {
+				subject = tpl.renderSubject({ title : 'This is the title of the mail', body : 'This is the body of the mail' });
+                body = tpl.renderBody({ title : 'This is the title of the mail', body : 'This is the body of the mail' });
+                expect(subject).to.equal('subject This is the title of the mail');
+                expect(body).to.equal('body This is the body of the mail');
+            } catch (err) {
+                assert.fail('we should not be there');
+            }
+            done();
+        });
+    });
+    
+    describe('Loading a template from a file :', function () {
+        before(function (done) {
+            jmcnetEmailTemplate.resetEmailTemplates();
+            done();
+        });
+        it('should be possible to load a template from a file', function (done) {
+            log.debug('Test Load template from a file');
+            var subject, body;
+            try {
+                var tpl = jmcnetEmailTemplate.loadEmailTemplateFromFile('template1', 'subject <%= title %>', 'test/emailTemplates/template1.html');
+                log.debug('tpl is "%s"', util.inspect(tpl));
+            
+				subject = tpl.renderSubject({ title : 'This is the title of the mail', body : 'This is the body of the mail' });
+                body = tpl.renderBody({ title : 'This is the title of the mail', body : 'This is the body of the mail' });
+                expect(subject).to.equal('subject This is the title of the mail');
+                expect(body).to.equal('<html>This is the body of the mail</html>');
+            } catch (err) {
+                assert.fail('we should not be there err='+err.message);
+            }
+            done();
+        });
+    });
+    
+    describe('Rendering a Date in locale String :', function () {
+        var tpl;
+        before(function (done) {
+            jmcnetEmailTemplate.resetEmailTemplates();
+            tpl = new jmcnetEmailTemplate.EmailTemplate('template1', 'The date is <%=: date | toLocaleDateString %>', 'body <%=body%>');
+            done();
+        });
+        it('should be possible to render the subject and body', function (done) {
+            log.debug('Test Render subject with date');
+            var subject;
+            var d = new Date(Date.parse('2014-08-31'));
+            subject = tpl.renderSubject({ date : d, body : 'This is the body of the mail' });
+            expect(subject).to.equal('The date is 31/08/2014');
+            subject = tpl.renderSubject({ date : d, body : 'This is the body of the mail' }, 'fr');
+            expect(subject).to.equal('The date is 31/08/2014');
+                subject = tpl.renderSubject({ date : d, body : 'This is the body of the mail' }, 'en');
+            expect(subject).to.equal('The date is 08/31/2014');
+            done();
+        });
+    });
+
+
+    after(function (done) {
+        // TODO do some after exit clean
+        done();
+    });
+});
